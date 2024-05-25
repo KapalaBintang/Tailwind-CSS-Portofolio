336f0ef2344cc0897198f58635d89266f245b6a1
Abdul Aziz <kapalabintang@gmail.com>
2024-05-25 21:17:24 +0700

    Portofolio using web

--- dist/img/main.js
+++ dist/img/main.js
@@ -17,39 +17,3 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
   hamburgerList.classList.toggle("hidden");
 });
-
-// SMTPJS API
-
-const form = document.getElementsByClassName("form-contact")[0];
-
-function sendEmail() {
-  Email.send({
-    SecureToken: "4614b777-7ff7-4136-9129-2b673fbf285a",
-    To: "bintangkapala@gmail.com",
-    From: "bintangkapala@gmail.com",
-    Subject: "This is the subject",
-    Body: `Name: ${document.getElementById("name").value} <br> Email: ${document.getElementById("email").value} <br> Message: ${document.getElementById("message").value}`,
-  }).then((message) => {
-    if (message == "OK") {
-      Swal.fire({
-        title: "Good job!",
-        text: "Successfully sent the message!",
-        icon: "success",
-      });
-    } else {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Something went wrong!",
-        footer: '<a href="#">Why do I have this issue?</a>',
-      });
-    }
-  });
-}
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("clicked");
-
-  sendEmail();
-});
