8ff6c113c66d0a0f7b8df012a038638106477af1
Abdul Aziz <kapalabintang@gmail.com>
2024-05-25 21:46:23 +0700

    Creating portofolio using Tailwind

---
+++ dist/img/main.js
@@ -0,0 +1,19 @@
+// ambil id humberger untuk menampilkan tombol
+
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+const hamburger = document.getElementById("hamburger");
+const hamburgerList = document.getElementById("hamburger-list");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  hamburgerList.classList.toggle("hidden");
+});
