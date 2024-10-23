6fd05c6526d71b917f9a32acd34be6981d53796f
Abdul Aziz <kapalabintang@gmail.com>
2024-10-23 17:14:20 +0700

    aaa

--- dist/img/main.js
+++ dist/img/main.js
@@ -20,38 +20,4 @@ hamburger.addEventListener("click", () => {
 
 // SMTPJS API
 
-const form = document.getElementsByClassName("form-contact")[0];
-
-function sendEmail() {
-  Email.send({
-    SecureToken: "4614b777-7ff7-4136-9129-2b673fbf285a",
-    To: "bintangkapala@gmail.com",
-    From: "bintangkapala@gmail.com",
-    Subject: "This is the subject",
-    Body: `Name: ${document.getElementById("name").value} <br> Email: ${document.getElementById("email").value} <br> Message: ${document.getElementById("message").value}`,
-  }).then((message) => {
-    if (message == "OK") {
-      Swal.fire({
-        title: "Good job!",
-        text: "Successfully sent the message!",
-        icon: "success",
-      });
-    } else {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Something went wrong!",
-        footer: '<a href="#">Why do I have this issue?</a>',
-      });
-    }
-  });
-}
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("clicked");
-
-  sendEmail();
-});
-
 // ambil id humberger untuk menampilkan tombol
