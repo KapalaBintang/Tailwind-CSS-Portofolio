095a09fa1ba532a1b11916c040bebfd28d2bd560
Abdul Aziz <bintangkapala@gmail.com>
2025-04-12 13:49:49 +0700

    chore(SEO): adjust image

--- app/page.tsx
+++ app/page.tsx
@@ -33,9 +33,7 @@ export default function Home() {
       <AboutSection />
       <ExperienceSection />
       <TechStack />
-
       <PortfolioSection />
-
       <ContactSection />
       <Footer />
     </main>
